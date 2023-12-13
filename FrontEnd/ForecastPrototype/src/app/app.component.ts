a3679b7020f29c029d949f24e5e2f3b44b598369
abhyasttripathi <abhyast.tripathi@nagarro.com>
2023-12-13 18:57:16 +0530

    IgniteUITrial

---
+++ FrontEnd/ForecastPrototype/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  localData = [
+    { Country: 'Country 1', Gender: 'Male', 'Age Group': '20-40', '2019': 4, '2020': 5, '2021': 3, '2022': 10, '2023': 3 },
+    { Country: 'Country 1', Gender: 'Male', 'Age Group': '20-40', '2019': 4, '2020': 5, '2021': 3, '2022': 10, '2023': 3 },
+    { Country: 'Country 1', Gender: 'Male', 'Age Group': '20-40', '2019': 4, '2020': 5, '2021': 3, '2022': 10, '2023': 3 },
+    { Country: 'Country 1', Gender: 'Male', 'Age Group': '20-40', '2019': 4, '2020': 5, '2021': 3, '2022': 10, '2023': 3 },
+    { Country: 'Country 1', Gender: 'Male', 'Age Group': '20-40', '2019': 4, '2020': 5, '2021': 3, '2022': 10, '2023': 3 },
+    { Country: 'Country 1', Gender: 'Male', 'Age Group': '20-40', '2019': 4, '2020': 5, '2021': 3, '2022': 10, '2023': 3 },
+    { Country: 'Country 1', Gender: 'Male', 'Age Group': '20-40', '2019': 4, '2020': 5, '2021': 3, '2022': 10, '2023': 3 },
+    // Add other data entries accordingly...
+  ];
+  
+title = 'ForecastPrototype';
+}
---
+++ FrontEnd/ForecastPrototype/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IgxGridModule } from 'igniteui-angular';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    IgxGridModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
